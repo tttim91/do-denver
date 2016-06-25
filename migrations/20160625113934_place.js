1cae3e08207c7b2f0fb1b7187e8537194d3ef0a0
Lucas Barbula <lucas.barbula@gmail.com>
2016-06-25 12:58:00 -0600

    seeds and migration change

--- migrations/20160625113934_place.js
+++ migrations/20160625113934_place.js
@@ -3,8 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('place', function(table){
     table.increments();
     table.text('name');
-    table.text('lat');
-    table.text('long');
+    table.float('lat', 4, 7);
+    table.float('long', 4, 7);
     table.text('description');
     table.text('image_url');
     table.integer('category_id').references('id').inTable('category').onDelete('cascade');
--- seeds/04_client_place.js
+++ seeds/04_client_place.js
@@ -1,13 +1,21 @@
+var find = require('../helper')
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      return Promise.all([
-        // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
-      ]);
-    });
+  return knex('client_place').del()
+    .then(function(){
+      return knex('client').select()
+      })
+        .then(function(clients) {
+          return knex('place').select()
+          .then(function (places) {
+            return Promise.all([
+              // Inserts seed entries
+              knex('client_place').insert({client_id: find.findClient('tttim', clients), place_id: find.findLocation('Tattered Cover Bookstore', places), have_visited: false}),
+              knex('client_place').insert({client_id: find.findClient('mabaile', clients), place_id: find.findLocation('Blue Pan Pizza', places), have_visited: true}),
+              knex('client_place').insert({client_id: find.findClient('wolfshark', clients), place_id: find.findLocation('Tattered Cover Bookstore', places), have_visited: true}),
+              knex('client_place').insert({client_id: find.findClient('lbarbula', clients), place_id: find.findLocation('Amethyst Coffee', places), have_visited: true})
+            ]);
+          });
+      })
 };
