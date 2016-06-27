ef1d73f8953b4f88f475b2909738b3e23e5c515e
Steven Lawson <unluckygamblermusic@gmail.com>
2016-06-27 11:53:58 -0600

    hashed passwords in client

--- seeds/01_client.js
+++ seeds/01_client.js
@@ -1,3 +1,6 @@
+var bcrypt = require('bcrypt');
+
+var hash = bcrypt.hashSync("password", 8);
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
@@ -5,10 +8,10 @@ exports.seed = function(knex, Promise) {
     .then(function(){
       return Promise.all([
         // Inserts seed entries
-        knex('client').insert({first_name: 'Tim', last_name: 'Musgrove', username: 'tttim', password: 'password'}),
-        knex('client').insert({first_name: 'Steven', last_name: 'Lawson', username:'wolfshark', password: 'password'}),
-        knex('client').insert({first_name: 'Mairin', last_name:'Bailey', username: 'mabaile', password: 'password'}),
-        knex('client').insert({first_name: 'Lucas', last_name:'Barbula', username: 'lbarbula', password: 'password'})
-      ]);    
+        knex('client').insert({first_name: 'Tim', last_name: 'Musgrove', username: 'tttim', password: hash}),
+        knex('client').insert({first_name: 'Steven', last_name: 'Lawson', username:'wolfshark', password: hash}),
+        knex('client').insert({first_name: 'Mairin', last_name:'Bailey', username: 'mabaile', password: hash}),
+        knex('client').insert({first_name: 'Lucas', last_name:'Barbula', username: 'lbarbula', password: hash})
+      ]);
     })
 };
