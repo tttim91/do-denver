d2d8b986e6e7282ee0d5aa6fe33624e300a1da89
Lucas Barbula <lucas.barbula@gmail.com>
2016-06-27 13:27:11 -0600

    $@

--- knexfile.js
+++ knexfile.js
@@ -6,5 +6,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/dodenver'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
 };
