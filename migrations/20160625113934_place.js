4a3a53e3f4b88e74c68666dd98f8aa086b478aaf
Tim Musgrove <tim.musgrove91@gmail.com>
2016-06-27 09:26:54 -0600

    Fixing seeds/migrations

--- migrations/20160625113934_place.js
+++ migrations/20160625113934_place.js
@@ -3,8 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('place', function(table){
     table.increments();
     table.text('name');
-    table.float('lat', 4, 7);
-    table.float('long', 4, 7);
+    table.float('lat', 10, 6);
+    table.float('lng', 10, 6);
     table.text('description');
     table.text('image_url');
     table.integer('category_id').references('id').inTable('category').onDelete('cascade');
--- seeds/04_client_place.js
+++ seeds/04_client_place.js
@@ -12,8 +12,14 @@ exports.seed = function(knex, Promise) {
             return Promise.all([
               // Inserts seed entries
               knex('client_place').insert({client_id: find.findClient('tttim', clients), place_id: find.findLocation('Tattered Cover Bookstore', places), have_visited: false}),
+              knex('client_place').insert({client_id: find.findClient('tttim', clients), place_id: find.findLocation('Blue Pan Pizza', places), have_visited: false}),
+              knex('client_place').insert({client_id: find.findClient('tttim', clients), place_id: find.findLocation('Amethyst Coffee', places), have_visited: true}),
+              knex('client_place').insert({client_id: find.findClient('tttim', clients), place_id: find.findLocation('Downpours Coffee', places), have_visited: true}),
               knex('client_place').insert({client_id: find.findClient('mabaile', clients), place_id: find.findLocation('Blue Pan Pizza', places), have_visited: true}),
+              knex('client_place').insert({client_id: find.findClient('mabaile', clients), place_id: find.findLocation("J's Noodles Star Thai", places), have_visited: true}),
+              knex('client_place').insert({client_id: find.findClient('mabaile', clients), place_id: find.findLocation('Tattered Cover Bookstore', places), have_visited: true}),
               knex('client_place').insert({client_id: find.findClient('wolfshark', clients), place_id: find.findLocation('Tattered Cover Bookstore', places), have_visited: true}),
+              knex('client_place').insert({client_id: find.findClient('wolfshark', clients), place_id: find.findLocation('Downpours Coffee', places), have_visited: true}),
               knex('client_place').insert({client_id: find.findClient('lbarbula', clients), place_id: find.findLocation('Amethyst Coffee', places), have_visited: true})
             ]);
           });
