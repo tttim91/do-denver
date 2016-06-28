b038dbed9feec7557189f36a3ed9e59030ecc7b6
Lucas Barbula <lucas.barbula@gmail.com>
2016-06-28 09:46:29 -0600

    rolledback migrations

--- routes/clients/client.js
+++ routes/clients/client.js
@@ -21,7 +21,7 @@ router.get('/sendData', function(req, res, next){
   })
 })
 router.post('/addPlace', function(req, res, next) {
-    knex('place').insert({name: req.body.name, lat:req.body.lat, lng:req.body.lng, description:"N/A", image_url:req.body.image_url, category_id: 20}).then(function() {
+    knex('place').insert({name: req.body.name, lat:req.body.lat, lng:req.body.lng, description:"N/A", image_url:req.body.image_url, category_id: 37}).then(function() {
         res.redirect('/');
     })
 })
