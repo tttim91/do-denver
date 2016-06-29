cc0d9a9fcc33fb4a7e4f8e1d7fcb0e6896a28037
Lucas Barbula <lucas.barbula@gmail.com>
2016-06-29 12:58:42 -0600

    working geocode

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -2,15 +2,12 @@ var NodeGeocoder = require('node-geocoder')
 
 var options = {
   provider: 'google',
-   httpAdapter: 'https',
+  httpAdapter: 'https',
   apiKey: 'AIzaSyCBzg50_Ei3s8Y6QWVBTzTz3imX-eVqGw8',
   formatter: null
 };
 
-var geocoder = NodeGeocoder(options);
-
 module.exports = {
-  convert: function (address){
-    geocoder.geocode(address)
-  }
+
+    geocoder: NodeGeocoder(options)
 }
