2485ee4ed0f8ee51dee6f5e4443c128b820f7a62
Tim Musgrove <tim.musgrove91@gmail.com>
2016-06-29 09:24:49 -0600

    Adding search features

--- routes/clients/search.js
+++ routes/clients/search.js
@@ -5,18 +5,30 @@ var db = require('../../db/api');
 var auth = require('../../auth');
 var request = require('request');
 
+function imageReferenceToImage(reference) {
+    console.log("reference", reference)
+    request("https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference="+reference+"&key=AIzaSyCBzg50_Ei3s8Y6QWVBTzTz3imX-eVqGw8", function(error, response, body) {
+        return body;
+    })
+
+}
+
 router.get('/search', auth.isNotLoggedIn, function(req, res, next){
+    if(req.query.category) {
         request("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=39.7645187,-104.9951978&radius=5000&type="+req.query.category+"&name="+req.query.query+"&key=AIzaSyCBzg50_Ei3s8Y6QWVBTzTz3imX-eVqGw8", function (error, response, body) {
             var answer = [];
             if (!error && response.statusCode == 200) {
                 var output = JSON.parse(body)
                 for(var i=0; i<output.results.length; i++) {
-                    console.log(output.results[i].name);
-                    answer.push({name: output.results[i].name, lat: output.results[i].geometry.location.lat, lng: output.results[i].geometry.location.lng, description: "N/A", image_url: output.results[0].photos[0].html_attributions[0], user_created: false, category_id: 20})
+                    answer.push({name: output.results[i].name, address: output.results[i].address, lat: output.results[i].geometry.location.lat, lng: output.results[i].geometry.location.lng, description: "N/A", image_url: "N/A", user_created: false, category_id: 20})
                 }
             }
+            console.log(answer);
             res.render('clients/search', {answer:answer, query: req.query.query, category: req.query.category});
         })
+    } else {
+        res.render('clients/search');
+    }
 
 });
 
